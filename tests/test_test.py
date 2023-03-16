bb2dfee7fc5ffa3012915c0f0a21cdbb9337c8f1
Yurii Hirchak <yuriyhirchak@gmail.com>
2023-03-16 17:26:05 +0200

    16.03

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,5 +1,10 @@
+from src.config.config import config
+
 def test_user_age_is_42():
     assert user.age == 43
 
 def test_user_age_is_50(user):
     assert user.age == 50
+
+def test_config():
+    print(config.get("BASE_URL"))
